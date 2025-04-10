5dfab754cea36e8ca7b6567e732484859f9a03db
Marco Lizano <lizanocr@hotmail.com>
2025-04-09 21:15:07 -0600

    using asserts

--- test/first_test.js
+++ test/first_test.js
@@ -1,14 +1,14 @@
 
-const {Builder, By, Key, until} = require('selenium-webdriver');
+const { Builder, By, Key, until } = require('selenium-webdriver');
 
-(async function example() { 
-     let driver = await new Builder().forBrowser('firefox').build();
-     try {
-         await driver.get('https://www.amazon.com');
+(async function example() {
+    let driver = await new Builder().forBrowser('firefox').build();
+    try {
+        await driver.get('https://www.amazon.com');
 
-     // Esperar hasta que el elemento con ID "leo_search_query_top" sea visible
-     //     await driver.wait(until.elementLocated(By.id("leo_search_query_top")), 10000);
-     //     await driver.wait(until.elementIsVisible(driver.findElement(By.id("leo_search_query_top"))), 10000);
+        // Esperar hasta que el elemento con ID "leo_search_query_top" sea visible
+        //     await driver.wait(until.elementLocated(By.id("leo_search_query_top")), 10000);
+        //     await driver.wait(until.elementIsVisible(driver.findElement(By.id("leo_search_query_top"))), 10000);
 
         await driver.wait(until.elementIsVisible(driver.findElement(By.id("twotabsearchtextbox"))), 10000);
         await driver.findElement(By.id("twotabsearchtextbox")).sendKeys('led Lámpara de pie', Key.RETURN);
@@ -16,9 +16,9 @@ const {Builder, By, Key, until} = require('selenium-webdriver');
         await driver.sleep(2000);
 
         let items = await driver.findElements(By.className("a-section a-spacing-none a-spacing-top-small s-title-instructions-style"));
-        
+
         for (let i = 0; i < items.length; i++) {
-            let text = await items[i].getText();        
+            let text = await items[i].getText();
             if (text.includes("Govee RGBIC")) {
 
                 await items[i].click();
@@ -26,7 +26,7 @@ const {Builder, By, Key, until} = require('selenium-webdriver');
                 await driver.sleep(2000);
 
                 await driver.wait(until.elementIsVisible(driver.findElement(By.id("add-to-wishlist-button-group"))), 10000);
-                
+
                 await driver.findElement(By.id("add-to-wishlist-button-group")).click();
                 console.log("Elemento agregado a la lista de deseos");
                 await driver.sleep(2000);
@@ -34,11 +34,11 @@ const {Builder, By, Key, until} = require('selenium-webdriver');
 
             }
         }
-     } finally {
+    } finally {
         await driver.quit();
         console.log("Cerrando el navegador");
-     }
- })();
+    }
+})();
 
 
 //  run with:   node test/first_test.js
---
+++ test/test_mocha.js
@@ -0,0 +1,45 @@
+
+const { Builder, By, Key, until } = require('selenium-webdriver');
+const assert = require('assert');
+
+async function example() {
+     let driver = await new Builder().forBrowser('firefox').build();
+
+     await driver.get('https://www.amazon.com');
+
+     await driver.sleep(3000);
+     await driver.wait(until.elementIsVisible(driver.findElement(By.id("twotabsearchtextbox"))), 10000);
+     await driver.findElement(By.id("twotabsearchtextbox")).sendKeys('gi joe', Key.RETURN);
+
+     await driver.sleep(3000);
+     let items = await driver.findElements(By.className("a-link-normal s-line-clamp-4 s-link-style a-text-normal"));
+
+     for (let i = 0; i < items.length; i++) {
+
+          let text = (await items[i].getText()).toString();
+
+          if (text.includes("Duke")) {
+
+               await items[i].click();
+               await driver.sleep(2000);
+
+               let title = await driver.findElement(By.id("title")).getText();
+
+               let description = title.toString();
+
+               
+               assert.ok(description.includes("Duke"));
+               break;
+               
+
+          }
+
+     }
+
+     await driver.quit();
+
+};
+
+example();
+
+//  run with:   node test/first_test.js
